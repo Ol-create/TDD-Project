ceebe45a920d1fdff64a0aed98cd4fdc4158d79f
Oluyemi Paul <oluola96@gmail,com>
2022-11-10 16:16:31 +0100

    add factorial method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,10 @@
 class Solver
+        def factorial(num)
+          return nil unless num.is_a? Integer
+          return 1 if num <= 0
+      
+          num * factorial(num - 1)
+        end
+      
+        
 end
