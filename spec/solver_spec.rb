dbb28e7ce70f6a6fa33095e1d5cb0b3bfabc1527
Oluyemi Paul <oluola96@gmail,com>
2022-11-10 16:05:05 +0100

    add test to check for  method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,20 @@
+describe Solver do
+    let(:solver) { Solver.new }
+  
+    describe 'has methods' do
+      it 'has a factorial method' do
+        expect(solver).to respond_to(:factorial)
+      end
+  
+      it 'has a reverse method' do
+        expect(solver).to respond_to(:reverse)
+      end
+  
+      it 'has a fizzbuzz method' do
+        expect(solver).to respond_to(:fizzbuzz)
+      end
+    end
+end
+
+  
+   
