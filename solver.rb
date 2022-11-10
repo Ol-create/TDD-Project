85a840a49b47f3c3da1fa94b503926be1af087fc
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-10 19:44:36 +0300

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,13 +1,12 @@
 class Solver
-        def factorial(num)
-          return nil unless num.is_a? Integer
-          return 1 if num <= 0
-      
-          num * factorial(num - 1)
-        end
-        def reverse(word)
-            word.reverse
-          end
-        
-          
+  def factorial(num)
+    return nil unless num.is_a? Integer
+    return 1 if num <= 0
+
+    num * factorial(num - 1)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
 end
